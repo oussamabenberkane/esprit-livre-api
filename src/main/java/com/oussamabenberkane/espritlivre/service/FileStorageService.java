aa1dcb84a7f1c7d32da7de7a6865439a69d3ee5e
Oussama Benberkane <oussamabenberkane.pro@gmail.com>
2026-01-24 07:13:40 +0100

    fix(fileStorage): add missing import

--- src/main/java/com/oussamabenberkane/espritlivre/service/FileStorageService.java
+++ src/main/java/com/oussamabenberkane/espritlivre/service/FileStorageService.java
@@ -1,6 +1,7 @@
 package com.oussamabenberkane.espritlivre.service;
 
 import com.oussamabenberkane.espritlivre.web.rest.errors.BadRequestAlertException;
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
